384f2074175edd26f60d715abac4818b121b7606
Zhao-yuha0 <337051328@qq.com>
2023-02-23 13:59:56 +0800

    Initial commit

---
+++ gee/router.go
@@ -0,0 +1,32 @@
+package gee
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc)}
+}
+
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	// %4s 代表输出的字符串占用4个字符的宽度，不足的用空格补齐
+	log.Printf("Route %4s - %s", method, pattern)
+	key := method + "-" + pattern
+	r.handlers[key] = handler
+}
+
+func (r *router) handle(c *Context) {
+	fmt.Println("router handle")
+	key := c.Method + "-" + c.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
